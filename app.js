30542dd746392384d215b89dcd7d7e6668cf112b
Juvygirl <67305871+Juvygirl@users.noreply.github.com>
2023-03-06 21:24:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,403 @@
-var firebaseConfig={apiKey:"AIzaSyB-vGEOyySC4JMYdtG1Ret4pbiZLmhknLs",authDomain:"jeanycar-25a07.firebaseapp.com",databaseURL:"https://jeanycar-25a07-default-rtdb.asia-southeast1.firebasedatabase.app/",projectId:"jeanycar-25a07",storageBucket:"jeanycar-25a07.appspot.com",messagingSenderId:"469828085729",appId:"1:469828085729:web:4bc30a96b8e227f0c9d4a0"};firebase.initializeApp(firebaseConfig);const internetTime=Date.now();var items=10,database=firebase.database(),sessionId=document.getElementById("session_id"),showFormButton=document.getElementById("show-form-button"),moreButton=document.getElementById("more-button"),notif=document.getElementById("header_notify"),numLikes=document.getElementById("numberLikes"),myForm=document.getElementById("postLink"),myContent=document.getElementById("column1"),myModal=document.getElementById("showmodal"),myModal_user=document.getElementById("m_username"),myModal_vistor=document.getElementById("m_visitor"),myModal_link=document.getElementById("m_link"),myModal_close=document.getElementById("m_close"),myModal_delete=document.getElementById("m_delete"),myModal_delete_id=document.getElementById("delete_id"),visitLink=document.getElementById("visit_link"),saveButton=document.getElementById("save-button"),cancelButton=document.getElementById("cancel-button"),titleInput=document.getElementById("title"),quoteTextarea=document.getElementById("quote"),authorInput=document.getElementById("author"),quoteTableBody=document.getElementById("quote-table-body"),postFrom=document.getElementById("addst");sessionStorage.setItem("data",internetTime);let inputChanged=!1;function clearForm(){document.getElementById("title").value="",document.getElementById("quote").value="",document.getElementById("author").value="",selectedRow=null}function openLink(e){window.open(e)}function loadDatabase(itemCount){database.ref("quotes").orderByChild("timestamp").limitToLast(itemCount).on("value",(function(snapshot){quoteTableBody.innerHTML="";var quotes=[];snapshot.forEach((function(e){var t=e.val();t.key=e.key,"true"===t.show&&quotes.push(t)})),quotes.reverse(),quotes.forEach((function(childData){var rrow=document.createElement("tr");let thStyle="";thStyle="Just now"==getTimeString(childData.timestamp)?"<td style='background-color: cornsilk;margin:10px'>":"<td style='background-color: white;margin:10px'>";let myViews=childData.views+" View"+(1==eval(childData.views)?"":"s"),modAuthor=childData.author,myTitle=childData.title,myQuote=childData.quote,myAuthor=modAuthor,timestamps=childData.timestamp,myTime=getTimeString(childData.timestamp);rrow.innerHTML=thStyle+"<div style='padding:13px'><span style='color:#ed4c2b;'><strong>"+myAuthor+"</strong></span><br><em style='color:#2c94fb;word-wrap: break-word;'><small>"+myTitle+"</em></small><br><br><small><pre>"+myQuote+"</pre></small><br><small><small style='color:#808080;'>"+myTime+"</small><br><span style='color:#008ba3;' align='center'>"+myViews+" </span></div></th>",quoteTableBody.appendChild(rrow),rrow.addEventListener("click",(function(){var counter=!1,modal=document.createElement("div");let myAuthor="<strong style='color:#ed4c2b;'>"+childData.author+"</strong>",myTitle="<em style='color:#2c94fb;word-wrap: break-word;'>"+childData.title+"</em>",tinyMargin="<small><small><br><br></small></small>",myViews="<span style='color:#808080'>"+childData.views+" visits | "+childData.views+" views</span>",dButton="<br><a class='delete-button'>Delete</a>",vButton="<button class='view-button'>VISIT</button>";modal.innerHTML="<center><div><p>"+myAuthor+"<br>"+myTitle+tinyMargin+myViews+"</div></div><br><br>"+vButton+dButton+"<div class='close-button'>AII</div>",modal.style.position="fixed",modal.style.top="50%",modal.style.left="50%",modal.style.width="300px",modal.style.height="auto",modal.style.transform="translate(-50%, -50%)",modal.style.backgroundColor="white",modal.style.padding="20px",modal.style.border="1px #aaa",modal.style.borderRadius="10px",modal.style.zIndex="9999";var closeButton=modal.querySelector(".close-button");closeButton.style.position="absolute",closeButton.style.top="108%",closeButton.style.left="44%",closeButton.style.fontSize="35px",closeButton.style.color="rgba(0, 0, 0, 0)",closeButton.style.cursor="pointer",closeButton.style.backgroundImage="url(icoCircle.svg)",closeButton.style.backgroundRepeat="no-repeat",closeButton.style.backgroundSize="40px 40px";var overlay=document.createElement("div");overlay.style.position="fixed",overlay.style.top="0",overlay.style.left="0",overlay.style.width="100%",overlay.style.height="100%",overlay.style.backgroundColor="rgba(0, 0, 0, 0.82)",overlay.style.zIndex="9998",closeButton.addEventListener("click",(function(){modal.remove(),overlay.remove()}));var viewButton=modal.querySelector(".view-button");viewButton.style.marginTop="5px",viewButton.style.marginBottom="15px",viewButton.style.fontWeight="bold",viewButton.style.borderRadius="15px",viewButton.style.width="200px";var deleteButton=modal.querySelector(".delete-button");deleteButton.style.color="#ccc",deleteButton.style.cursor="pointer",viewButton.addEventListener("click",(function(){openLink(childData.title),database.ref("quotes/"+childData.key).update({views:eval(childData.views)+eval(1)})})),closeButton.addEventListener("click",(function(){modal.remove(),overlay.remove()})),deleteButton.addEventListener("click",(function(){counter?(database.ref("quotes/"+childData.key).update({show:"false"}),modal.remove(),overlay.remove()):(deleteButton.innerHTML="&#x2713; Confirm",counter=!0)})),document.body.appendChild(modal),document.body.appendChild(overlay)}))}))}),(function(e){console.error("Failed to retrieve quotes:",e),"PERMISSION_DENIED"===e.code?notif.innerHTML="Database is locked":"NETWORK_ERROR"===e.code?notif.innerHTML="No internet connection.":notif.innerHTML="Failed to save link."}))}function saveData(e,t,o){(inputChanged=!1,saveButton.disabled=!0,""===e||""===o)?saveButton.disabled=!1:(t.match(/\n/g)||[]).length>5?(saveButton.disabled=!1,quoteTextarea.setCustomValidity("Too many line breaks")):(event.preventDefault(),saveButton.disabled=!1,t.length>100&&(t=t.substring(0,100)),e.length>80&&(e=e.substring(0,80)),o.length>50&&(o=o.substring(0,50)),database.ref("quotes").push().set({title:e,quote:t,author:o,sessionkey:internetTime,show:"true",views:"0",visits:"0",timestamp:firebase.database.ServerValue.TIMESTAMP},(function(e){e?(console.error("Failed to save quote:",e),notif.style.display="block",a,"PERMISSION_DENIED"===e.code?notif.innerHTML="Database is locked":"NETWORK_ERROR"===e.code?notif.innerHTML="No internet connection.":notif.innerHTML="Failed to save link."):(myForm.style.display="none",postFrom.reset(),showFormButton.style.display="block",myContent.style.display="block",notif.style.display="block",notif.innerHTML="Link saved!")})))}function loadData(e){var t=sessionStorage.getItem(e);return t||0}showFormButton.addEventListener("click",(function(){showFormButton.style.display="none",myContent.style.display="none",myForm.style.display="block",notif.style.display="none"})),cancelButton.addEventListener("click",(function(){inputChanged=!1,myForm.style.display="none",myContent.style.display="block",showFormButton.style.display="block",postFrom.reset()})),saveButton.addEventListener("click",(function(e){saveData(titleInput.value,quoteTextarea.value,authorInput.value)})),loadDatabase(items),titleInput.addEventListener("input",(function(){inputChanged=""!==titleInput})),window.addEventListener("beforeunload",(function(e){inputChanged&&(e.preventDefault(),e.returnValue="")})),moreButton.addEventListener("click",(function(){loadDatabase(items+=5)}));
+// Initialize Firebase
+var firebaseConfig = {
+	apiKey: "AIzaSyB-vGEOyySC4JMYdtG1Ret4pbiZLmhknLs",
+	authDomain: "jeanycar-25a07.firebaseapp.com",
+	databaseURL: "https://jeanycar-25a07-default-rtdb.asia-southeast1.firebasedatabase.app/",
+	projectId: "jeanycar-25a07",
+	storageBucket: "jeanycar-25a07.appspot.com",
+	messagingSenderId: "469828085729",
+	appId: "1:469828085729:web:4bc30a96b8e227f0c9d4a0",
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the Firebase Realtime Database
+const internetTime = Date.now();
+
+var items = 10;
+var database = firebase.database();
+
+// Get DOM elements
+
+var sessionId = document.getElementById('session_id');
+
+var showFormButton = document.getElementById('show-form-button');
+
+var moreButton = document.getElementById('more-button');
+
+var notif = document.getElementById('header_notify');
+
+var numLikes = document.getElementById('numberLikes');
+
+var myForm = document.getElementById('postLink');
+var myContent = document.getElementById('column1');
+
+var myModal = document.getElementById('showmodal');
+var myModal_user = document.getElementById('m_username');
+var myModal_vistor = document.getElementById('m_visitor');
+var myModal_link = document.getElementById('m_link');
+
+var myModal_close = document.getElementById('m_close');
+var myModal_delete = document.getElementById('m_delete');
+var myModal_delete_id = document.getElementById('delete_id');
+
+var visitLink = document.getElementById("visit_link");
+
+var saveButton = document.getElementById('save-button');
+var cancelButton = document.getElementById('cancel-button');
+
+var titleInput = document.getElementById('title');
+var quoteTextarea = document.getElementById('quote');
+var authorInput = document.getElementById('author');
+var quoteTableBody = document.getElementById('quote-table-body');
+
+var postFrom = document.getElementById('addst');
+
+sessionStorage.setItem("data", internetTime);
+
+let inputChanged = false;
+
+// Show form when the "Show Form" button is clicked
+showFormButton.addEventListener('click', function() {
+	showFormButton.style.display = 'none';
+	myContent.style.display = 'none';
+
+	myForm.style.display = 'block';
+	notif.style.display = "none";
+});
+
+// Hide form when the "Cancel" button is clicked
+cancelButton.addEventListener('click', function() {
+	inputChanged = false;
+	myForm.style.display = 'none';
+	myContent.style.display = 'block';
+	showFormButton.style.display = 'block';
+	postFrom.reset();
+});
+
+// Save form data to Firebase Realtime Database when the "Save" button is clicked
+// Save form data to Firebase Realtime Database when the "Save" button is clicked
+
+saveButton.addEventListener('click', function(event) {
+	var title = titleInput.value;
+	var quote = quoteTextarea.value;
+	var author = authorInput.value;
+	saveData(title, quote, author);
+});
+
+
+
+
+loadDatabase(items);
+
+// Clear form inputs and selected row
+function clearForm() {
+	document.getElementById('title').value = '';
+	document.getElementById('quote').value = '';
+	document.getElementById('author').value = '';
+	selectedRow = null;
+}
+
+
+
+
+function openLink(url) {
+	window.open(url);
+	//document.getElementById("sup").value = eval(upvote) + eval(1);
+	//document.forms["frm2"].submit();
+
+}
+
+titleInput.addEventListener('input', function() {
+	if (titleInput === '') {
+		inputChanged = false;
+	} else {
+		inputChanged = true;
+	}
+});
+
+window.addEventListener('beforeunload', function(e) {
+	if (inputChanged) {
+		e.preventDefault();
+		e.returnValue = '';
+	}
+});
+
+
+moreButton.addEventListener('click', function() {
+	items += 5;
+	loadDatabase(items);
+
+});
+
+function loadDatabase(itemCount) {
+	database.ref('quotes').orderByChild('timestamp').limitToLast(itemCount).on('value', function(snapshot) {
+		// Clear existing table rows
+		quoteTableBody.innerHTML = '';
+		// Generate new table rows in reverse order
+		var quotes = [];
+		snapshot.forEach(function(childSnapshot) {
+			var childData = childSnapshot.val();
+			childData.key = childSnapshot.key;
+			if (childData.show === 'true') {
+				quotes.push(childData); // Fix here: push childData instead of quote
+			}
+		});
+
+		quotes.reverse(); // Reverse the order of the quotes
+		quotes.forEach(function(childData) {
+
+			var rrow = document.createElement('tr')
+
+			//////////////////////////////////////////////////////////
+
+
+			let thStyle = "";
+
+
+			if ((getTimeString(childData.timestamp)) == "Just now") {
+				thStyle = "<td style='background-color: cornsilk;margin:10px'>";
+			} else {
+				thStyle = "<td style='background-color: white;margin:10px'>";
+			}
+
+			let myViews = childData.views + " View" + (eval(childData.views) == 1 ? "" : "s");
+
+			let modAuthor = childData.author;
+			let myTitle = childData.title;
+			let myQuote = childData.quote;
+			let myAuthor = modAuthor;
+			let timestamps = (childData.timestamp);
+
+			let myTime = getTimeString(childData.timestamp);
+
+			/////////////////////////////////////////////////////////
+
+			rrow.innerHTML =
+				thStyle + "<div style='padding:13px'><span style='color:#ed4c2b;'><strong>" + myAuthor +
+				"</strong></span><br><em style='color:#2c94fb;word-wrap: break-word;'><small>" + myTitle +
+				"</em></small><br><br><small><pre>" + myQuote +
+				"</pre></small><br><small><small style='color:#808080;'>" + myTime +
+				"</small><br><span style='color:#008ba3;' align='center'>" + myViews + " </span></div></th>";
+
+			/////////////////////////////////////////////////////////
+
+			quoteTableBody.appendChild(rrow);
+
+			// Add event listener to delete quote on row click
+			// Add event listener to delete quote on row click
+			rrow.addEventListener("click", function() {
+				// Create modal with delete button and close button
+
+				var counter = false;
+				var modal = document.createElement('div');
+
+
+				let myAuthor = "<strong style='color:#ed4c2b;'>" + childData.author + "</strong>";
+				let myTitle = "<em style='color:#2c94fb;word-wrap: break-word;'>" + childData.title + "</em>";
+				let tinyMargin = "<small><small><br><br></small></small>";
+				let myViews = "<span style='color:#808080'>" + childData.views + " visits | " + childData.views + " views</span>";
+
+				let dButton = "<br><a class='delete-button'>Delete</a>";
+				let vButton = "<button class='view-button'>VISIT</button>";
+				
+				modal.innerHTML = "<center><div><p>" + myAuthor + "<br>" + myTitle + tinyMargin + myViews +
+				"</div></div><br><br>"+ vButton +  dButton + "<div class='close-button'>AII</div>";
+				
+				modal.style.position = 'fixed';
+				modal.style.top = '50%';
+				modal.style.left = '50%';
+				modal.style.width = '300px';
+				modal.style.height = 'auto';
+				modal.style.transform = 'translate(-50%, -50%)';
+
+				modal.style.backgroundColor = 'white';
+
+				modal.style.padding = '20px';
+				modal.style.border = '1px #aaa';
+				modal.style.borderRadius = '10px';
+				modal.style.zIndex = '9999';
+
+				// Style close button
+				var closeButton = modal.querySelector('.close-button');
+				closeButton.style.position = 'absolute';
+				closeButton.style.top = '108%';
+				closeButton.style.left = '44%'; 
+				closeButton.style.fontSize = '35px';
+				closeButton.style.color = 'rgba(0, 0, 0, 0)'; 
+				closeButton.style.cursor = 'pointer';
+
+				closeButton.style.backgroundImage = 'url(icoCircle.svg)';
+				closeButton.style.backgroundRepeat = 'no-repeat';
+				closeButton.style.backgroundSize = '40px 40px';
+
+
+
+				// Add overlay with grey background
+				var overlay = document.createElement('div');
+				overlay.style.position = 'fixed';
+				overlay.style.top = '0';
+				overlay.style.left = '0';
+				overlay.style.width = '100%';
+				overlay.style.height = '100%';
+				overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.82)';
+				overlay.style.zIndex = '9998';
+
+				// Add event listener to close button
+				closeButton.addEventListener('click', function() {
+					modal.remove();
+					overlay.remove();
+				});
+
+
+				var viewButton = modal.querySelector('.view-button');
+				viewButton.style.marginTop = '5px';
+				viewButton.style.marginBottom = '15px';
+
+				viewButton.style.fontWeight = 'bold';
+				viewButton.style.borderRadius = '15px';
+				viewButton.style.width = '200px';
+
+				var deleteButton = modal.querySelector('.delete-button');
+				
+				deleteButton.style.color = '#ccc';
+				deleteButton.style.cursor = 'pointer';
+			
+				viewButton.addEventListener('click', function() {
+					openLink(childData.title);
+					database.ref('quotes/' + childData.key).update({
+						views: eval(childData.views) + eval(1)
+					});
+				});
+
+				closeButton.addEventListener('click', function() {
+					modal.remove();
+					overlay.remove();
+				});
+
+				// Delete quote from database when delete button is clicked
+				deleteButton.addEventListener('click', function() {
+
+					if(!counter){
+						deleteButton.innerHTML = "&#x2713; Confirm";
+						counter = true;
+					}else{
+
+					database.ref('quotes/' + childData.key).update({
+						show: 'false'
+					});
+
+					modal.remove();
+					overlay.remove();
+
+					}
+
+				});
+
+				// Add modal and overlay to the page
+				document.body.appendChild(modal);
+				document.body.appendChild(overlay);
+			});
+
+		});
+
+	}, function(error) {
+		console.error("Failed to retrieve quotes:", error);
+		if (error.code === "PERMISSION_DENIED") {
+			//alert("You don't have permission to save quotes.");
+			notif.innerHTML = "Database is locked";
+		} else if (error.code === "NETWORK_ERROR") {
+			//alert("No internet connection. Please check your network settings and try again.");
+			notif.innerHTML = "No internet connection.";
+		} else {
+			//alert("Failed to save quote. Please try again later.");
+			notif.innerHTML = "Failed to save link.";
+		}
+	});
+
+
+}
+
+
+function saveData(title, quote, author) {
+	inputChanged = false;
+	saveButton.disabled = true;
+
+	if (title === '' || author === '') {
+		saveButton.disabled = false;
+	} else {
+		var lineBreakCount = (quote.match(/\n/g) || []).length;
+		if (lineBreakCount > 5) {
+			saveButton.disabled = false;
+			quoteTextarea.setCustomValidity("Too many line breaks");
+		} else {
+			event.preventDefault();
+			saveButton.disabled = false;
+			if (quote.length > 100) {
+				quote = quote.substring(0, 100);
+			}
+
+			if (title.length > 80) {
+				title = title.substring(0, 80);
+			}
+
+			if (author.length > 50) {
+				author = author.substring(0, 50);
+			}
+
+			// Save form data to Firebase Realtime Database
+			var quoteRef = database.ref('quotes').push();
+			quoteRef.set({
+				title: title,
+				quote: quote,
+				author: author,
+				sessionkey: internetTime,
+				show: "true",
+				views: "0",
+				visits: "0",
+				timestamp: firebase.database.ServerValue.TIMESTAMP
+			}, function(error) {
+				if (error) {
+					console.error("Failed to save quote:", error);
+					notif.style.display = "block";
+					a
+
+					if (error.code === "PERMISSION_DENIED") {
+						//alert("You don't have permission to save quotes.");
+						notif.innerHTML = "Database is locked";
+					} else if (error.code === "NETWORK_ERROR") {
+						//alert("No internet connection. Please check your network settings and try again.");
+						notif.innerHTML = "No internet connection.";
+					} else {
+						//alert("Failed to save quote. Please try again later.");
+						notif.innerHTML = "Failed to save link.";
+					}
+				} else {
+					myForm.style.display = 'none';
+					postFrom.reset();
+
+
+					showFormButton.style.display = 'block';
+					myContent.style.display = 'block';
+
+					notif.style.display = "block";
+					notif.innerHTML = "Link saved!";
+				}
+
+			});
+
+		}
+
+	}
+}
+
+
+function loadData(item) {
+	var data = sessionStorage.getItem(item);
+	if (data) {
+		return data;
+	} else {
+		return 0;
+	}
+}
